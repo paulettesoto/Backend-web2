56568fcab2de14050001544bd33eb577a71332d6
paulette <esmesotoangeles863@gmail.com>
2023-10-09 23:20:26 -0700

    09-10-2023

---
+++ send.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+
+pywhatkit.sendwhatmsg('+526682430712', 'Holi esto es enviado desde python', 16,27)
---
+++ uploadfile.py
@@ -0,0 +1,5 @@
+from Google import Create_Service
+
+CLIENT_SECRET_FILE = 'client_secret_GoogleCloudDemo.json'
+API_NAME = 'drive'
+API_VERSION =
