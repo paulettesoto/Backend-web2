f522130e9bcc0aab16e3bc4ff567c66e37a8fcab
paulette <esmesotoangeles863@gmail.com>
2023-12-01 15:31:47 -0700

    01-12-2023

--- doctor_app/routers/sendMessage.py
+++ doctor_app/routers/sendMessage.py
@@ -12,4 +12,11 @@ router = APIRouter(
 
 @router.post("/sendMessage")
 async def send(phoneN:str, message:str, hour:int, min:int):
-    pywhatkit.sendwhatmsg('+52'+phoneN, message, hour,min, 15,tab_close=True, close_time=1)
+    try:
+        pywhatkit.sendwhatmsg('+52' + phoneN, message, hour, min, 15, tab_close=True, close_time=1)
+    # Código que utiliza pywhatkit
+    except KeyError:
+        # Manejar la excepción, por ejemplo, imprimir un mensaje o tomar una acción alternativa
+        print("No se encontró la variable de entorno 'DISPLAY'. Verifica si estás en un entorno gráfico.")
+
+
