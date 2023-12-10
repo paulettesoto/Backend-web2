8c6489d1a51121981a48cf04e6a0d869ecd0ec38
paulette <esmesotoangeles863@gmail.com>
2023-12-10 02:59:55 -0700

    09-12-2023

--- doctor_app/routers/uploadImages.py
+++ doctor_app/routers/uploadImages.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import tempfile
 from fastapi import APIRouter, File, UploadFile
 
 router = APIRouter(
@@ -11,29 +12,23 @@ router = APIRouter(
 paciente = 'paciente1'
 path = os.path.abspath(os.path.join('G:', 'Mi unidad', 'pruebasAgendado', paciente, 'img'))
 
-
 @router.post("/image")
 async def image(image: UploadFile = File(...)):
     print(image.filename)
 
-    # Asegurar que el directorio de destino exista
-    os.makedirs(path, exist_ok=True)
+    # Usar un directorio temporal
+    temp_dir = tempfile.mkdtemp()
+    temp_file_path = os.path.join(temp_dir, image.filename)
 
-    # Usar rutas absolutas
-    source = os.path.abspath(os.path.join("doctor_app/routers/img", image.filename))
-    destination = os.path.join(path, image.filename)
-
-    with open(destination, "wb") as buffer:
-        shutil.copyfileobj(image.file, buffer)
-
-    uploadimage(source, destination)
-    return {"filename": image.filename}
-
-
-def uploadimage(source, destination):
     try:
-        shutil.move(source, destination)
-    except FileNotFoundError:
-        print(f"El archivo {source} no se pudo encontrar.")
-    except Exception as e:
-        print(f"Ocurrió un error al mover el archivo: {e}")
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(image.file, buffer)
+
+        # Mover el archivo desde el directorio temporal al destino final
+        destination = os.path.join(path, image.filename)
+        shutil.move(temp_file_path, destination)
+
+        return {"filename": image.filename}
+    finally:
+        # Limpiar el directorio temporal incluso en caso de error
+        shutil.rmtree(temp_dir, ignore_errors=True)
