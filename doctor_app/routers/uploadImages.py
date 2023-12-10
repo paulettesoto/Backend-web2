0b661e26083595980547424bf81b0b99fb73259c
paulette <esmesotoangeles863@gmail.com>
2023-12-10 03:26:39 -0700

    09-12-2023

--- doctor_app/routers/uploadImages.py
+++ doctor_app/routers/uploadImages.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from tempfile import NamedTemporaryFile
 from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import JSONResponse
 
@@ -10,21 +11,26 @@ router = APIRouter(
 )
 
 paciente = 'paciente1'
-# Define la ruta local en Google Drive
-path = os.path.join('G:', 'Mi unidad', 'pruebasAgendado', paciente, 'img')
+# Utiliza el directorio temporal proporcionado por Render
+temp_path = os.environ.get("TMPDIR", "/tmp")  # Obtén la ubicación del directorio temporal
+final_path = os.path.abspath(os.path.join('G:', 'Mi unidad', 'pruebasAgendado', paciente, 'img'))
 
 @router.post("/image")
 async def image(image: UploadFile = File(...)):
     try:
+        # Crea un archivo temporal para almacenar el archivo cargado
+        with NamedTemporaryFile(delete=False, dir=temp_path) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(image.file, temp_file)
+
         # Construye la ruta local del archivo en Google Drive
-        destination = os.path.join(path, image.filename)
+        destination = os.path.join(final_path, image.filename)
 
         # Asegúrate de que el directorio de destino exista
         os.makedirs(os.path.dirname(destination), exist_ok=True)
 
-        # Guarda el archivo en la ruta local
-        with open(destination, "wb") as buffer:
-            shutil.copyfileobj(image.file, buffer)
+        # Mueve el archivo desde el directorio temporal al destino final
+        shutil.move(temp_file_path, destination)
 
         return JSONResponse(content={"filename": image.filename}, status_code=200)
     except Exception as e:
