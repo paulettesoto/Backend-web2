89c25e14e10fb8ada74537e786b35934e0496420
paulette <esmesotoangeles863@gmail.com>
2023-12-10 05:31:37 -0700

    09-12-2023

--- doctor_app/routers/uploadImages.py
+++ doctor_app/routers/uploadImages.py
@@ -1,10 +1,7 @@
-import os, shutil
-from fastapi import APIRouter, File, UploadFile, Request
+import os
+import shutil
+from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import JSONResponse
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
 
 router = APIRouter(
     prefix="/uploadImages",
@@ -13,43 +10,21 @@ router = APIRouter(
 )
 
 paciente = 'paciente1'
+# Define la ruta local en Google Drive
 path = os.path.join('G:', 'Mi unidad', 'pruebasAgendado', paciente, 'img')
-
-# Ruta al archivo de credenciales JSON
-credentials_path = 'credentials.json'
-
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
-
-creds = None
-token_path = 'token_drive.json'
-
-if os.path.exists(token_path):
-    creds = Credentials.from_authorized_user_file(token_path)
-
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
-        creds = flow.run_local_server(port=0)
-
-    with open(token_path, 'w') as token:
-        token.write(creds.to_json())
-
-drive_service = build('drive', 'v3', credentials=creds)
 
 @router.post("/image")
 async def image(image: UploadFile = File(...)):
     try:
+        # Construye la ruta local del archivo en Google Drive
         destination = os.path.join(path, image.filename)
+
+        # Asegúrate de que el directorio de destino exista
         os.makedirs(os.path.dirname(destination), exist_ok=True)
 
+        # Guarda el archivo en la ruta local
         with open(destination, "wb") as buffer:
             shutil.copyfileobj(image.file, buffer)
-
-        file_metadata = {'name': image.filename}
-        media = MediaFileUpload(destination, mimetype='image/jpeg')
-        drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
         return JSONResponse(content={"filename": image.filename}, status_code=200)
     except Exception as e:
