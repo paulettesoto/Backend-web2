d2f1a3c56a19e6576f5b40550dd1be5590199101
paulette <esmesotoangeles863@gmail.com>
2023-12-10 04:42:07 -0700

    09-12-2023

--- doctor_app/routers/uploadImages.py
+++ doctor_app/routers/uploadImages.py
@@ -1,8 +1,10 @@
-import os
-import shutil
-from tempfile import NamedTemporaryFile
-from fastapi import APIRouter, File, UploadFile
+import os, shutil
+from fastapi import APIRouter, File, UploadFile, Request
 from fastapi.responses import JSONResponse
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 
 router = APIRouter(
     prefix="/uploadImages",
@@ -11,26 +13,43 @@ router = APIRouter(
 )
 
 paciente = 'paciente1'
-# Utiliza el directorio temporal proporcionado por Render
-temp_path = os.environ.get("TMPDIR", "/tmp")  # Obtén la ubicación del directorio temporal
-final_path = os.path.abspath(os.path.join('G:', 'Mi unidad', 'pruebasAgendado', paciente, 'img'))
+path = os.path.join('G:', 'Mi unidad', 'pruebasAgendado', paciente, 'img')
+
+# Ruta al archivo de credenciales JSON
+credentials_path = 'credentials.json'
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+
+creds = None
+token_path = 'token_drive.json'
+
+if os.path.exists(token_path):
+    creds = Credentials.from_authorized_user_file(token_path)
+
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+        creds = flow.run_local_server(port=0)
+
+    with open(token_path, 'w') as token:
+        token.write(creds.to_json())
+
+drive_service = build('drive', 'v3', credentials=creds)
 
 @router.post("/image")
 async def image(image: UploadFile = File(...)):
     try:
-        # Crea un archivo temporal para almacenar el archivo cargado
-        with NamedTemporaryFile(delete=False, dir=temp_path) as temp_file:
-            temp_file_path = temp_file.name
-            shutil.copyfileobj(image.file, temp_file)
-
-        # Construye la ruta local del archivo en Google Drive
-        destination = os.path.join(final_path, image.filename)
-
-        # Asegúrate de que el directorio de destino exista
+        destination = os.path.join(path, image.filename)
         os.makedirs(os.path.dirname(destination), exist_ok=True)
 
-        # Mueve el archivo desde el directorio temporal al destino final
-        shutil.move(temp_file_path, destination)
+        with open(destination, "wb") as buffer:
+            shutil.copyfileobj(image.file, buffer)
+
+        file_metadata = {'name': image.filename}
+        media = MediaFileUpload(destination, mimetype='image/jpeg')
+        drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
         return JSONResponse(content={"filename": image.filename}, status_code=200)
     except Exception as e:
