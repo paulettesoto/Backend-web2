02fae9f34fd842afcd872dafc5275170fd212a85
paulette <esmesotoangeles863@gmail.com>
2023-12-10 01:04:25 -0700

    09-12-2023

--- doctor_app/routers/patientdoctors.py
+++ doctor_app/routers/patientdoctors.py
@@ -43,8 +43,8 @@ def buscar_doc(especialidad:str):
 def favorites(idPaciente: str):
     connect, cursor = connection()
     try:
-        query = ("select DISTINCT  c.Doctor_idDoctor, d.Nombre, d.PrimerApe, d. SegundoApe, d.Especialidad "
-                 "from cita as c "
+        query = ("select DISTINCT  c.Doctor_idDoctor, d.Nombre, d.PrimerApe, d.SegundoApe, d.Especialidad, d.Cedula, "
+                 "d.Celular, d.Foto from cita as c "
                  "INNER JOIN doctor as d on d.idDoctor=c.Doctor_idDoctor "
                  "where c.Paciente_idPaciente=" + idPaciente + " and account='Y';")
         cursor.execute(query)
@@ -53,13 +53,15 @@ def favorites(idPaciente: str):
         if records:
             favorites_list = []
             for record in records:
-                idDoctor, nombre, apellido1, apellido2, especialidad = record
+                idDoctor, nombre, apellido1, apellido2, especialidad, cedula, celular = record
                 favorites_dict = {
                     "id": idDoctor,
                     "Nombre": nombre,
                     "PrimerApe": apellido1,
                     "SegundoApe": apellido2,
-                    "especialidad": especialidad
+                    "especialidad": especialidad,
+                    "cedula": cedula,
+                    "celular": celular
                 }
                 favorites_list.append(favorites_dict)
 
